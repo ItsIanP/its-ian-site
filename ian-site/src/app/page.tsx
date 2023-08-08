8b6b6ed534a5e97fce7000f7f7c2d45f40eea8be
Ian Powe <ip20914@gmail.com>
2023-08-08 11:50:19 -0400

    Initial commit for site

---
+++ ian-site/src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./globals.css"
+
+export default function Home() {
+  return (
+    <main >
+      <div>
+        <h1 className={"page_title"}>This is my Site!</h1>
+      </div>
+    </main>
+  )
+}
